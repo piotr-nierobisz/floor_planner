8f26c5895e4077dff0c27b8fae856b728bc332c9
piotr <hello@piotr.cx>
2024-12-04 12:27:24 +0000

    Init repository

---
+++ DOM/building.js
@@ -0,0 +1,27 @@
+const Building = {
+    name: 'Building',
+    base_url:"http://localhost:9191",
+    houses:[{
+        id: "AAA",
+        name:"A101",
+        area:"60",
+        floor:"3",
+        floors:[
+            {
+                id:"xxx",
+                name:"Floor 1",
+                image:"floor-1.jpg"
+            },
+            {
+                id:"yyy",
+                name:"Floor 2",
+                image:"floor-2.jpg"
+            },
+            {
+                id:"zzz",
+                name:"Floor 3",
+                image:"floor-3.jpg"
+            }
+        ]
+    }],
+}
